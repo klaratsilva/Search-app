817c2392b98121aa4e1681b7d0c592864ca1856e
klara silva <klaratsilva@gmail.com>
2020-11-27 15:03:10 +0000

    style updates

--- src/pages/users.js
+++ src/pages/users.js
@@ -6,38 +6,39 @@ const Users = ({ userData, reposData }) => {
 
     return (
         <>
-            { userData.length !== 0 ? <div className='page'>
+            <div className='page'>
                 <div className='container'>
-                    {/*      <div className='row'> */}
-                    <div className='user-container'>
-                        <div className='left'>
-                            <div className='user-picture'>
-                                <img src={userData.avatar_url} alt={userData.name}></img>
+                    {userData.length !== 0 ?
+                        <div className='user-container'>
+                            <div className='left'>
+                                <div className='user-picture'>
+                                    <img src={userData.avatar_url} alt={userData.name}></img>
+                                </div>
+                                <div className='user-name'>
+                                    <h2 className='user-name-text'>{userData.name} </h2>
+                                </div>
+                                <div className='user-repos'>
+                                    <h4 className='user-repos-text'>{`The  total number of repositories is ${userData.public_repos}`} </h4>
+                                </div>
                             </div>
-                            <div className='user-name'>
-                                <h2 className='user-name-text'>{userData.name} </h2>
-                            </div>
-                            <div className='user-repos'>
-                                <h4 className='user-repos-text'>{`The  total number of repositories is ${userData.public_repos}`} </h4>
-                            </div>
-                        </div>
-                        <div className='right'>
-                            <div className='user-repos-list'>
-                                <h2>List of repositories:</h2>
-                                <ul>
+                            <div className='right'>
+                                <div className='user-repos-list'>
+                                    <h2 className='user-repos-headline'>List of repositories:</h2>
+                                    <ul>
 
-                                    {reposData.map((data) => (
-                                        <>
-                                            <li key={data.id}> <h4>{data.name}</h4> <p>{data.description}</p> </li>
-                                        </>
-                                    ))}
-                                </ul>
+                                        {reposData.map((data) => (
+                                            <>
+                                                <li key={data.id}> <h4>{data.name}</h4> <p>{data.description}</p> </li>
+                                            </>
+                                        ))}
+                                    </ul>
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        </div > :
+                        <div className='error-message'><h1>Please try another username</h1></div>
+                    }
                 </div>
-
-            </div > : <h1>Please try another username</h1>}
+            </div>
         </>
     );
 };
